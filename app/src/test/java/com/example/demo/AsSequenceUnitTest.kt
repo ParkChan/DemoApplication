cf9a8c4bdfd3aa721186fa08fe193755c6340526
ParkChan <hydrarkk@naver.com>
2022-05-24 01:53:10 +0900

    [테스트 코드 추가]  최종본으로 하나의 커밋으로 작업
    커밋테스트1
    
    커밋테스트12
    
    커밋테스트123

---
+++ app/src/test/java/com/example/demo/AsSequenceUnitTest.kt
@@ -0,0 +1,42 @@
+package com.example.demo
+
+import org.junit.Test
+
+/**
+ * Kotlin Collections : Eager evaluation
+ * Kotlin Sequences : Lazy evaluation
+ */
+class AsSequenceUnitTest {
+
+    @Test
+    fun `Eager evaluation 는 수행해야 할 연산이 있으면 미루지 않고 바로 처리하는 방식`() {
+
+        val fruits = listOf("apple", "banana", "kiwi", "cherry")
+
+        fruits.filter {
+            println("checking the length of $it")
+            it.length > 5
+        }
+            .map {
+                println("mapping to the length of $it")
+                "${it.length}"
+            }
+            .toList()
+
+    }
+
+    @Test
+    fun `Lazy evaluation은 지금 하지 않아도 되는 연산은 최대한 뒤로 미루는 방식`() {
+        val fruits = listOf("apple", "banana", "kiwi", "cherry")
+        fruits.asSequence()
+            .filter {
+                println("checking the length of $it")
+                it.length > 5
+            }
+            .map {
+                println("mapping to the length of $it")
+                "${it.length}"
+            }
+            .toList()
+    }
+}
