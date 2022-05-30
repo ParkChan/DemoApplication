bcdf014106ee83073b863ed508521bc4b7b2aaa6
ParkChan <hydrarkk@naver.com>
2022-05-31 01:35:32 +0900

    [추가/테스트 코드] Event Wrapper

---
+++ app/src/test/java/com/example/demo/EventTest.kt
@@ -0,0 +1,40 @@
+package com.example.demo
+
+import com.example.demo.fragment.common.model.Event
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class EventTest {
+
+    private lateinit var event: Event<Boolean>
+
+    @Before
+    fun setup() {
+        event = Event(value = true)
+    }
+
+    @Test
+    fun `최초 접근 시_데이터를 전달합니다`() {
+        // given
+
+        // when
+        val actual = event.isActive()
+
+        // then
+        assertTrue(actual)
+    }
+
+    @Test
+    fun `두 번 이상 접근 시_데이터를 전달하지 않습니다`() {
+        // given
+        event.isActive()
+
+        // when
+        val actual = event.isActive()
+
+        // then
+        assertFalse(actual)
+    }
+}
