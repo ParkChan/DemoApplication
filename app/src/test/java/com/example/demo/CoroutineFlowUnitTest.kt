d7ee9de1c205038b4cab4df8910c5db47d57a839
ParkChan <hydrarkk@naver.com>
2022-07-10 22:42:03 +0900

    [테스트코드/추가] conflate collect vs collectLastest

---
+++ app/src/test/java/com/example/demo/CoroutineFlowUnitTest.kt
@@ -0,0 +1,38 @@
+package com.example.demo
+
+import com.example.demo.util.InstantExecutorExtension
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(InstantExecutorExtension::class)
+class CoroutineFlowUnitTest {
+
+    val flow = flow<Int> {
+        for (i in 0..10) {
+            emit(i)
+            delay(100)
+        }
+    }
+
+    @Test
+    fun `데이터를 발행하는 시간보다 처리하는 시간이 더 긴 경우 collectLastest를 사용하면 마지막 데이터만이 소비됩니다`() = runBlocking {
+        flow.collectLatest {
+            delay(1_000)
+            println("number >> $it")
+        }
+    }
+
+    @Test
+    fun `한 번 시작된 데이터 소비는 끝날때까지 하고, 데이터 소비가 끝난 시점의 가장 최신 데이터를 다시 소비됩니다`() = runBlocking {
+
+        flow.onEach {
+            println("number >> emit $it")
+        }.conflate().collect {
+            delay(5_000)
+            println("number >> consume $it")
+        }
+    }
+}
