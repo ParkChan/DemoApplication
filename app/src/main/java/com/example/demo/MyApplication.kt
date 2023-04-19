8470c951ae92051237c4b7c385f6167e3a4e6158
iron883 <hydrarkk@naver.com>
2023-04-19 23:56:54 +0900

    [추가] Hilt 추가

--- app/src/main/java/com/example/demo/MyApplication.kt
+++ app/src/main/java/com/example/demo/MyApplication.kt
@@ -2,8 +2,10 @@ package com.example.demo
 
 import android.app.Application
 import com.facebook.stetho.Stetho
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class MyApplication: Application() {
 
     override fun onCreate() {
