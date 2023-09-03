4d4a71d7c2fc0a141c31e5f3e5f62423b6bff7ba
ParkChan <hydrarkk@naver.com>
2023-09-03 20:49:35 +0900

    [수정] ItemTouchHelperCallback, ItemTouchHelperListener 소스 위치 이동

--- app/src/main/java/com/example/demo/ui/activity/ItemTouchHelperActivity.kt
+++ app/src/main/java/com/example/demo/ui/activity/ItemTouchHelperActivity.kt
@@ -125,4 +125,40 @@ class ItemTouchHelperActivity : AppCompatActivity() {
             }
         }
     }
+
+    class ItemTouchHelperCallback(private val listener: ItemTouchHelperListener) : ItemTouchHelper.Callback() {
+
+        override fun getMovementFlags(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder
+        ): Int {
+            val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
+            val swipeFlags = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+            return makeMovementFlags(dragFlags, swipeFlags) // swipeFlags 대신 0 으로 설정시 스와이프 처리X
+        }
+
+
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return listener.onItemMove(viewHolder.bindingAdapterPosition, target.bindingAdapterPosition)
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            listener.onItemSwipe(viewHolder.bindingAdapterPosition)
+        }
+
+        override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+            super.onSelectedChanged(viewHolder, actionState)
+            listener.onDropDown()
+        }
+    }
+    interface ItemTouchHelperListener {
+        fun onItemMove(startPosition: Int, endPosition: Int): Boolean
+        fun onItemSwipe(position: Int)
+        fun onDropDown()
+    }
 }
+
