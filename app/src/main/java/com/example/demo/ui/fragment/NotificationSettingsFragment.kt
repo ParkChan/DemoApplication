37cafc00e23901b753d353a01c4d7fab54e47721
ParkChan <hydrarkk@naver.com>
2023-11-07 23:22:43 +0900

    [추가] 알림권한 예제코드 작성

--- app/src/main/java/com/example/demo/ui/fragment/NotificationSettingsFragment.kt
+++ app/src/main/java/com/example/demo/ui/fragment/NotificationSettingsFragment.kt
@@ -10,7 +10,6 @@ import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat.checkSelfPermission
