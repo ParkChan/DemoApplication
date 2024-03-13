1eb88852208a1a80cbb31fd9c05fc4c92cbc10e0
ParkChan <hydrarkk@naver.com>
2024-03-14 00:42:12 +0900

    [추가] 리스트 아이템 텍스트 사이즈 지정하기

--- app/src/main/java/com/example/demo/ui/fragment/viewpager2/TwoFragment.kt
+++ app/src/main/java/com/example/demo/ui/fragment/viewpager2/TwoFragment.kt
@@ -1,5 +1,6 @@
 package com.example.demo.ui.fragment.viewpager2
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +10,10 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.demo.databinding.FragmentTwoBinding
+import com.example.demo.databinding.ItemCarLeftBinding
+import com.example.demo.ui.fragment.RecyclerLeftHoldScrollTestFragment
 import com.example.demo.ui.viewmodel.ViewPagerViewModel
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -19,6 +23,17 @@ class TwoFragment : Fragment() {
 
     private lateinit var binding: FragmentTwoBinding
     private val viewModel: ViewPagerViewModel by activityViewModels()
+    private val adapter: RecyclerAdapter by lazy {
+        RecyclerAdapter()
+    }
+
+    private val mockItems: List<RecyclerLeftHoldScrollTestFragment.Car> by lazy {
+        (1..3000).map {
+            RecyclerLeftHoldScrollTestFragment.Car(
+                it.toString().plus("번 차량"),""
+            )
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,11 +48,65 @@ class TwoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Timber.d("lifecycle is onViewCreated")
+
         viewLifecycleOwner.lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.systemEvent.collect {
                     Timber.d("systemEvent is $it")
                 }
+            }
+        }
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        binding.rvList.adapter = adapter
+        adapter.replaceItems(mockItems)
+
+    }
+
+    internal class RecyclerAdapter :
+        RecyclerView.Adapter<RecyclerAdapter.SampleViewHolder>() {
+
+        private val itemList = mutableListOf<RecyclerLeftHoldScrollTestFragment.Car>()
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SampleViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ItemCarLeftBinding.inflate(inflater, parent, false)
+            return SampleViewHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: SampleViewHolder, position: Int) {
+            holder.bind(position, itemList[position])
+        }
+
+        override fun getItemCount(): Int {
+            return itemList.size
+        }
+
+        @SuppressLint("NotifyDataSetChanged")
+        internal fun replaceItems(item: List<RecyclerLeftHoldScrollTestFragment.Car>) {
+            itemList.run {
+                clear()
+                addAll(item)
+                notifyDataSetChanged()
+            }
+        }
+
+        internal class SampleViewHolder(
+            private val binding: ItemCarLeftBinding
+        ) : RecyclerView.ViewHolder(binding.root) {
+            fun bind(position: Int, car: RecyclerLeftHoldScrollTestFragment.Car) {
+                binding.car = car
+                binding.tvTest.textSize = position.toFloat()
+                binding.tvTest.tag?.let {
+                    Timber.d("CHAN >>> getTag $it")
+                } ?: run {
+                    Timber.d("CHAN >>> getTag is NULL")
+                }
+                binding.tvTest.tag = position
+
+                binding.executePendingBindings()
             }
         }
     }
