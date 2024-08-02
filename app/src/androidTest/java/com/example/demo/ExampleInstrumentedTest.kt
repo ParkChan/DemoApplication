b52adb41262a11be2ec41e064828c76c471f2fc4
iron883 <hydrarkk@naver.com>
2024-08-03 01:34:38 +0900

    [추가] 테스트 샘플 코드 추가

--- app/src/androidTest/java/com/example/demo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/demo/ExampleInstrumentedTest.kt
@@ -1,24 +1,31 @@
 package com.example.demo
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.ext.junit.rules.activityScenarioRule
+import com.example.demo.screen.MainScreen
+import com.example.demo.ui.MainActivity
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ExampleInstrumentedTest : TestCase() {
+
+    @get:Rule
+    val activityRule = activityScenarioRule<MainActivity>()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.demoapplication", appContext.packageName)
+    fun useAppContext() = run {
+        step("이동 테스트 ") {
+            MainScreen {
+                viewpagerButton {
+                    isVisible()
+                    click()
+                }
+            }
+        }
     }
 }
